a16c31dc34e0f5d8857311e0505e6d7a8a25962c
ehy0414 <djagusdyd78@gmail.com>
2025-03-19 10:22:49 +0900

    db json 연동 axios통신

--- src/pages/spring/SpringBreakPage.jsx
+++ src/pages/spring/SpringBreakPage.jsx
@@ -1,6 +1,8 @@
 "use client";
 import styled from "styled-components";
 import BreakSelector from "../../modules/springPage/break/BreakSelector";
+import { useEffect, useState } from "react";
+import api from "../../services/axios";
 
 const Wrapper = styled.div`
   position: absolute;
@@ -9,6 +11,22 @@ const Wrapper = styled.div`
 `;
 
 function SpringBreakPage() {
+    const [springBreak, setSpringBreak] = useState("");
+
+    // json-server version
+    const getBreak = async() => {
+        try {
+            const response = await api.get("/spring");
+            setSpringBreak(response.data);
+            console.log("debug >> axios get OK!! ", response.data);
+        } catch(err) {
+            console.log( err );
+        }
+    }
+
+    useEffect( () => {
+        getBreak();
+    }, [] );
 
   return (
     <Wrapper>
