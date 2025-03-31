32cf6ec82bf2d9d8538f340c54a64caba85b9b57
ehy0414 <djagusdyd78@gmail.com>
2025-03-31 12:43:33 +0900

    로고 변경

--- src/components/layout/header/Logo.jsx
+++ src/components/layout/header/Logo.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import logo from "./logo/계절따라 로고.png";
 
 const LogoWrapper = styled.div`
   align-self: stretch;
@@ -19,7 +20,7 @@ const LogoImage = styled.img`
   aspect-ratio: 1;
   object-fit: contain;
   object-position: center;
-  width: 40px;
+  width: 60px;
   cursor: pointer;
 `;
 
@@ -28,7 +29,7 @@ export function Logo() {
   return (
     <LogoWrapper>
       <LogoImage
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/1f2d4dc7a1b2a005f35d9a87c7f0465a8b6a70291b01caf763fd95a2d1365606?placeholderIfAbsent=true&apiKey=7adddd5587f24b91884c2915be4df62c"
+        src={logo}
         alt="Company Logo"
         onClick={() => {navigate("/")}}
       />
