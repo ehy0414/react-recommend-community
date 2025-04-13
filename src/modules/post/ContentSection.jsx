d88e513ba7abb475df4ae9a56f2bc9c24e649075
ehy0414 <djagusdyd78@gmail.com>
2025-04-13 15:01:50 +0900

    :sparkles: feat: 삭제기능 추가

--- src/modules/post/ContentSection.jsx
+++ src/modules/post/ContentSection.jsx
@@ -1,12 +1,42 @@
 "use client";
 import styled from "styled-components";
+import api from "../../services/axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ContentSection = ( props ) => {
+  const [userId, setUserId] = useState(null); // 세션에서 가져온 사용자 ID
+  const navigate = useNavigate();
+  // console.log(props.data.id);
+
+  // 세션에서 userId를 가져오는 로직
+  useEffect(() => {
+    setUserId(localStorage.getItem("userId"));
+  }, []);
+
+  // 게시글 삭제 핸들러
+  const deletePost = async () => {
+    if (window.confirm("정말로 이 게시글을 삭제하시겠습니까?")) {
+      try{
+        await api.delete(`/${props.data.season}/${props.data.id}`);
+        alert("삭제가 완료되었습니다!");
+        navigate("/");
+      }
+      catch (err) {
+        console.error(err);
+      }
+      
+    }
+  };
   return (
     <Container>
       <ContentBackground>
         <ContentWrapper>
             <Heading>{props.data.title}</Heading>
+            {/* 게시글 삭제 버튼 */}
+            {userId === props.data.userId && (
+              <DeleteButton onClick={deletePost}>삭제하기</DeleteButton>
+            )}
           <ContentContainer>
             <Description>
               {props.data.content}
@@ -97,6 +127,31 @@ const Description = styled.p`
   line-height: 30px;
   @media (max-width: 991px) {
     max-width: 100%;
+  }
+`;
+
+const DeleteButton = styled.button`
+  padding: 10px 20px;
+  align-items: center;
+  text-align: center;
+  font-family: 'Gamja Flower';
+  font-size: 17px;
+  font-weight: 600;
+  white-space: nowrap;
+  line-height: 1;
+  cursor: pointer;
+  color:gray;
+  border-radius: 50px;
+  border: 1px solid lightgray;
+  background-color: rgb(255, 255, 255);
+  margin-left: 60%;
+  
+  @media (max-width: 991px) {
+    white-space: initial;
+  }
+  &:hover {
+    transform: scale(1.1); /* 호버 시 크기 확대 */
+    color: red;
   }
 `;
 
